90ce43431c64b53b7fb438ee81f369776d3f5269
Rum <tylerdownie0@gmail.com>
2020-08-20 22:43:40 -0700

    switch to function based

--- merge_sort.rb
+++ merge_sort.rb
@@ -10,170 +10,203 @@ def merge_sort(array)
 
     new_combo = []
 
-    until sorted 
+    combinations = merging(combinations, new_combo, result)
 
-        if combinations.size == 1
-
-            sorted = true
-
-        end
-
-        if combinations[0].size > 1
-
-            new_combo = new_combo || []
-
-            resort = []
-
-            leftside = nil
-
-            rightside = nil
-
-            lvalue = nil
-
-            rvalue = nil
-
-            while true
-
-                if combinations.length == 1
-
-                    combinations = combinations[0]
-
-                    break
-
-                end
-
-                if leftside == [] && lvalue == nil
-
-                    if rvalue != nil
-
-                        if rvalue > rightside[0]
-
-                            resort << rightside.shift
-                        
-                        end
-
-                        resort << rvalue
-
-                    end
-
-                    if rightside != []
-
-                        resort.concat(rightside)
-
-                    end
-
-                    new_combo.concat(resort)
-
-                    break
-
-                elsif combinations[0] != [] && resort == []
-
-                    leftside = combinations.shift
-
-                    lvalue = leftside.shift
-
-                end
-
-                if rightside == [] && rvalue == nil
-
-                    if lvalue != nil
-
-                        if lvalue > leftside[0]
-
-                            resort << leftside.shift
-                        
-                        end
-
-                        resort << lvalue
-
-                    elsif leftside != []
-
-                        resort.concate(leftside)
-
-                    end
-
-                    new_combo.concat(resort)
-
-                    break
-
-                elsif combinations[1] != [] && resort == []
-
-                    rightside = combinations.shift
-
-                    rvalue = rightside.shift
-
-                end
-            
-                if lvalue <= rvalue
-    
-                    resort << lvalue
-
-                    lvalue = leftside.shift
-
-                elsif lvalue > rvalue
-
-                    resort << rvalue
-                        
-                    rvalue = rightside.shift
-
-                else
-
-                    result.concat(resort)
-
-                end
-            end
-
-            if resort != []
-
-                result << resort
-
-            end
-
-            if combinations == []
-
-                combinations = result
-
-            end
-
-            
-
-        else
-
-            combinations.each do |x|
-                
-                if x[1] == nil || x[0] < x[1] 
-
-                    next  
-                    
-                else
-
-                    x[0], x[1] = x[1], x[0]
-
-                end
-
-            end
-        end
-
-    end
-
-    p combinations
+    p combinations, "here now, exited merging"
 
 end
 
 def sides(array, size, combinations)
 
-    if array.length <= 2 
+    p array
+
+    if array.length <= 2
 
         combinations << array
+        
+        p combinations, "combo"
 
     else
 
         left = sides(array[0..(size/2 - 1)], size / 2, combinations)
 
-        right = sides(array[size/2..size-1], size / 2, combinations)
+        right = sides(array[size/2..size-1], size / 2), combinations)
 
+    end
+
+    return combinations
+
+end
+
+def merging(combinations, new_combo, result)
+
+    p combinations[0].size, "here"
+
+    if combinations[0].size > 1
+
+        new_combo = new_combo || []
+
+        resort = []
+
+        leftside = nil
+
+        rightside = nil
+
+        lvalue = nil
+
+        rvalue = nil
+
+        while true
+
+            p "here 1"
+
+            if combinations.length == 1 && resort == []
+
+                combinations
+
+                break
+
+            end
+
+            if leftside == [] && lvalue == nil
+
+                p "here 2"
+
+                if rvalue != nil
+
+                    if rvalue > rightside[0]
+
+                        resort << rightside.shift
+                    
+                    end
+
+                    resort << rvalue
+
+                end
+
+                if rightside != []
+
+                    resort.concat(rightside)
+
+                end
+
+                new_combo.concat(resort)
+
+                p new_combo
+
+                break
+
+            elsif combinations[0] != [] && resort == []
+
+                p "here for loading the lvalue."
+
+                leftside = combinations.shift
+
+                lvalue = leftside.shift
+
+            end
+
+            if rightside == [] && rvalue == nil
+
+                if lvalue != nil
+
+                    if lvalue > leftside[0]
+
+                        resort << leftside.shift
+                    
+                    end
+
+                    resort << lvalue
+
+                elsif leftside != []
+
+                    resort.concate(leftside)
+
+                end
+
+                new_combo.concat(resort)
+
+                p new_combo
+
+                break
+
+            elsif combinations[1] != [] && resort == []
+
+                p "here for loading rvalue"
+
+                rightside = combinations.shift
+
+                rvalue = rightside.shift
+
+            end
+
+            lvalue, rvalue, resort, result = compare(lvalue, rvalue, leftside, rightside, result, resort)
+        
+
+        end
+
+        if resort != []
+
+            result << resort
+
+        end
+
+        if combinations == []
+
+            combinations = result
+
+        end
+
+        merging(combinations, new_combo, result)
+
+    else
+
+        combinations.each do |x|
+            
+            if x[1] == nil || x[0] < x[1] 
+
+                next  
+                
+            else
+
+                x[0], x[1] = x[1], x[0]
+
+            end
+
+        end
     end
 
 end
 
-merge_sort([2,3,4,6,4,8,17,9,10])
+
+def compare(lvalue, rvalue, leftside, rightside, result, resort)
+
+    if lvalue <= rvalue
+
+        p "here lvalue <= rvalue"
+    
+        resort << lvalue
+
+        lvalue = leftside.shift
+
+    elsif lvalue > rvalue
+
+        p "here rvalue < lvalue"
+
+        resort << rvalue
+            
+        rvalue = rightside.shift
+
+    else
+
+        p "Neither <= or < rvalue and lvalue"
+
+        result.concat(resort)
+
+    end
+
+    return lvalue, rvalue, resort, result
+
+end
+
+merge_sort([2,3,4,6,4,8,17,100,9,13,14])
